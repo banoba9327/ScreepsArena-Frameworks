b51f51f3c5a84a7712825931d778809e936866f2
banoba9327 <banoba930527@gmail.com>
2022-04-21 00:06:28 +0900

    オブジェクト型の型名取得関数をユーティリティとして追加

---
+++ src/Utils/SystemUtility.mjs
@@ -0,0 +1,13 @@
+
+/**
+ * 対象の型名を返す。
+ * @param {Object} target 型名を取り出す対象。
+ * @returns {string} 型名
+ * @link https://pisuke-code.com/javascript-get-exact-data-type/
+ */
+export function getTypeNameOf(target)
+{
+    var funcNameRegex = /function (.{1,})\(/;
+    var results = (funcNameRegex).exec((target).constructor.toString());
+    return (results && results.length > 1) ? results[1] : "";
+}
