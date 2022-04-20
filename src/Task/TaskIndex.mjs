fdf38074f497da75b3329455b7335103c1bdd32c
banoba9327 <banoba930527@gmail.com>
2022-04-21 00:46:40 +0900

    Task関連クラスについてまとめてインポートするための名前空間用ファイル追加

---
+++ src/Task/TaskIndex.mjs
@@ -0,0 +1,15 @@
+// MEMO : フレームワーク外部から参照するクラスを一括でインポートするためのソースコード
+
+import { Task } from "./Task.mjs";
+import { CaptureFlag } from "./Creep/CaptureFlag.mjs";
+import { Move } from "./Creep/Move.mjs";
+import { MeleeAttack } from "./Creep/Attack/MeleeAttack.mjs";
+
+export const prototypes = {
+    Task: Task,
+    creep: {
+        Move: Move,
+        MeleeAttack: MeleeAttack,
+        CaptureFlag: CaptureFlag,
+    }
+}
